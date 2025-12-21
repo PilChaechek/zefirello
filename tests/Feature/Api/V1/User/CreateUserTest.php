f486e698383bf1dfea85ce950ba30f310fc44bc1
Gleb Neverov <mnogobuk@gmail.com>
2025-12-21 22:09:53 +0700

    refactor(database): исправлен неймспейс PDO в конфиге БД
    test(users): обновлен тест создания пользователя для использования ролей и сидинга

--- tests/Feature/Api/V1/User/CreateUserTest.php
+++ tests/Feature/Api/V1/User/CreateUserTest.php
@@ -2,24 +2,36 @@
 
 namespace Tests\Feature\Api\V1\User;
 
+use Database\Seeders\RolesAndAdminSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CreateUserTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
     public function test_can_create_user()
     {
-        $admin = \App\Models\User::factory()->create();
+        // Сначала наполняем базу ролями
+        $this->seed(RolesAndAdminSeeder::class);
 
+        // Создаем админа, от имени которого будем выполнять действие
+        $admin = \App\Models\User::factory()->create();
+        $admin->assignRole('admin');
+
+        // Выполняем запрос на создание нового пользователя
         $response = $this->actingAs($admin)->postJson('/api/v1/users', [
             'name' => 'New Guy',
             'email' => 'new@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'role' => 'user',
         ]);
 
+        // Проверяем результат
         $response->assertStatus(201); // 201 Created
         $this->assertDatabaseHas('users', ['email' => 'new@example.com']);
     }
